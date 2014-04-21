cf5aa7828aa3c3489e5d7c05b5b7e0f5d619d977
Nicholas Hinds <hindsn@gmail.com>
2014-04-21 21:45:51 +1200

    Allow purging old mdns::service entries from /etc/avahi/services

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,12 +3,22 @@
 # This class includes the common components for mdns installation
 #
 # Parameters:
+#   $purge_services:
+#     Whether to remove existing service files from /etc/avahi/services.
+#     Defaults to false.
 #
 # Actions:
 #
 # Sample Usage:
 #   include mdns
 #
-class mdns {
+class mdns($purge_services=false) {
   package {['avahi-daemon', 'avahi-utils']:}
+
+  file {'/etc/avahi/services':
+    ensure => directory,
+    recurse => true,
+    purge => $purge_services,
+    require => Package['avahi-daemon'];
+  }
 }
